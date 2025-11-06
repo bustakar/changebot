518ef0150ba6d1fcae3a82dd513eeeb450b0d8e7
Karel Busta <karelbusta@gmail.com>
2025-11-06 18:56:39 +0100

    feat: Replace timeline with changelog view

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { Timeline } from './components/Timeline';
+import { Changelog } from './components/Changelog';
 
 export const dynamic = 'force-dynamic';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Timeline />
+      <Changelog />
     </div>
   );
 }
